c8c685ab83e98f8bb2bfe286535a4f333454c060
Anant <anant312004@gmail.com>
2024-10-14 22:54:10 +0530

    Added database classes

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
 
 
 #------------ Initialisations ---------------#
@@ -17,8 +19,50 @@ app.app_context().push()
 class professional(db.Model):
 
     __tablename__ = "professional"
-    professional_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     date_created = db.Column(db.Date, nullable=False)
     description = db.Column(db.String, nullable=False)
-    # service_type = 
+    service_type = db.Column()
+    experience = db.Column(db.Integer, nullable=False)
+    request = db.Column(db.Boolean, default=None)
+
+    service = relationship("service", backref=backref("professionals", lazy=True))
+
+class customer(db.Model):
+
+    __tablename__ = "customer"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email_id = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    fullname = db.Column(db.String, nullable=False)
+    address = db.Column(db.String, nullable=False)
+    pin_code = db.Column(db.Integer, nullable=False)
+
+
+class service(db.Model):
+    
+    __tablename__ = "service"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String, nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    time_required = db.Column(db.Date, nullable=False)
+    description = db.Column(db.String, nullable=False)
+
+    professionals = relationship("professional", backref=backref("service", lazy=True))
+
+
+class service_reqest(db.Model):
+
+    __tablename__ = "service_request"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    service_id = db.Column(db.Integer, ForeignKey('service.id'), nullable=False)
+    customer_id = db.Column(db.Integer, ForeignKey('customer.id'), nullable=False)
+    professional_id = db.Column(db.Integer, ForeignKey('professional.id'), nullable=False)
+    date_of_request = db.Column(db.Date, nullable=False)
+    date_of_completion = db.Column(db.Date, default=False)
+    service_status = db.Column(db.String, default=False)
+    rstars = db.Column(db.Integer, default=False)
+    rfeedback = db.Column(db.String, default=False)
+
+
